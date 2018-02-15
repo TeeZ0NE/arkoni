9472c40d83dab658e7334cff8cf64412fc767e68
Vadim <vadim@hyperweb.com.ua>
2018-02-15 16:01:53 +0200

    adding external admin scripts and required autoload Jq library

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,26 @@ let mix             = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.autoload({
+  jquery: ['$', 'window.jQuery', 'jQuery'],
+  moment: 'moment'
+});
+mix.js(['resources/assets/js/app.js',
+  'resources/assets/js/admin.js'], 'public/js/app.js')
    .stylus('resources/assets/stylus/app.styl', 'public/css');
+  
+mix.webpackConfig({
+    resolve: {
+      modules: [
+        'node_modules'
+      ],
+      alias: {
+        'vue$': 'vue/dist/vue.js',
+        jquery: 'jquery/src/jquery'
+      }
+    }
+ });
 if (mix.inProduction()) {
     mix.version();
-}   
+} 
    

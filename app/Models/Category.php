ecfe3fcfb77c9dca62e20a87d0b265b3c4a23c90
Vadim <vadim@hyperweb.com.ua>
2018-02-15 15:47:00 +0200

    adding models Brand, Attribute, Category

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+  protected $fillable = array(
+    'name',
+    'url_slug',
+    'parent_id'
+  );
+  public $timestamps  = false;
+
+  public function setNameAttribute($value)
+  {
+    return $this->attributes['name'] = mb_strtolower($value);
+  }
+
+  public function sub_cat()
+  {
+    return $this->belongsTo(Category::class,'parent_id','id');
+  }
+
+  public function parent_cat()
+  {
+    return $this->hasMany(Category::class,'id','parent_id');
+  }
+
+}

ef654f34fa8862b07392a34403fdf25b71fde536
Vadim <vadim@hyperweb.com.ua>
2018-02-15 15:54:08 +0200

    Main admin controller for logging

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,6 +24,6 @@ class AdminController extends Controller
      */
     public function index(Request $request)
     {
-        return view('layouts.admin.admin');
+        return view('admin.admin');
     }
 }

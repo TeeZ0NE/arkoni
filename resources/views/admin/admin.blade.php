56af9571d00a44ac00e2f4347215b8ca540350ae
Vadim <vadim@hyperweb.com.ua>
2018-02-19 11:02:25 +0200

    Moved here message block with errors or successful. Added scrf token

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -6,20 +6,39 @@
   <meta name="description" content="@yield('description')">
   <meta name="keywords" content="@yield('keywords')">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> --}}
-  </script>
-  <script src="{{ mix('js/app.js') }}"></script>
-  <style type="text/css" href="{{ mix('css/app.css') }}"></style> 
-  {{-- styles --}}
-  @include('admin.parts.components.styles')
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+</script>
+<script src="{{ mix('js/app.js') }}"></script>
+<style type="text/css" href="{{ mix('css/app.css') }}"></style> 
+{{-- styles --}}
+@include('admin.parts.components.styles')
 </head>
 <body>
   <div class="container">
     <p class="row my-auto p-2 alert alert-dark">Панель <strong>Адміністратора!</strong><i class="fab fa-accusoft"></i></p>
     <div class="row">@include('admin.parts.components.main_nav')</div>
-    @section('admin_main_content')@show
-    <div class="row">@include('admin.parts.footer')</div>
-  </div>
-</body>
-</html>
+    {{-- showing errors and messages --}}
+    <div class="row my-auto">
+      @if (Session::has("msg"))
+      @component('layouts.parts.components.alert',
+        ["alert_class"=>"success",
+        "alert_text"=>Session::get("msg")])
+        @endcomponent
+        @endif
+
+        @isset($sort) @php $sort or 'acs'@endphp @endisset
+
+        @if($errors->any())
+        @component('layouts.parts.components.alert',
+          ["alert_class"=>"warning",
+          "alert_text"=>$errors->first()])
+          @endcomponent
+          @endif
+        </div>
+
+        @section('admin_main_content')@show
+        <div class="row">@include('admin.parts.footer')</div>
+      </div>
+    </body>
+    </html>
 

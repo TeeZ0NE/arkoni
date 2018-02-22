b0830a7adfaa026b2e57ffcf99d497a19c506f37
Vadim <vadim@hyperweb.com.ua>
2018-02-22 12:05:45 +0200

    add scripts 4 workin when adding new items: creating new fields, removing them

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -33,4 +33,34 @@ $(function(){
         // old_id=id;
         $("#parent-select").val(sub_id).change();
       })
+  // add category checkbox
+    $('.add_cat').on('click',function(event){
+        event.preventDefault();
+        var id = $('#cats').val();
+        if (id=='') return;
+        var opt_text = $('#cats :selected').text();
+        $('#cat_block').append('<input type="checkbox" name="categories[]" value="'+id+'" checked class="ml-1">'+opt_text);
+    })
+
+    // add input field for adding attributes and own values
+    $('.add_attr').on('click',function(event){
+        event.preventDefault();
+        var id = $('#attrs').val();
+        if (id=='') return;
+        var opt_text = $('#attrs :selected').text();
+        $('#attr_block').append('<div class="input-group mb-1"><div class="input-group-prepend"><span class="input-group-text">'+opt_text+'</span></div><input type="text" class="form-control" placeholder="Параметри" name="values[]" aria-label="'+opt_text+'" aria-describedby="item-name"><input type="hidden" name="attrs[]" value="'+id+'"><a href="#" class="btn btn-danger remove_attr" onclick="javascript:void(0);"><i class="fas fa-trash-alt"></a></div>');
+    });
+
+    // remove input field from attributes
+    $(document).on('click','a.remove_attr',function(event){
+        event.preventDefault();
+        $(this).parent().remove();
+    })
+
+    // change label on getting file in admin
+    $('#img_upload').on('change',function(){
+        var file_name = $(this).val().split('\\').slice(-1)[0];
+        $(this).next('.custom-file-label').html(file_name);
+    })
+
 });

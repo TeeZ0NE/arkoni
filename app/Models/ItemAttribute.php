fdc85c185fc750f4aba90e6f2080c4747bd4e863
Vadim <vadim@hyperweb.com.ua>
2018-02-23 11:40:08 +0200

    adding 2 methods to take attributes and values of item with and without name of attribute

--- app/Models/ItemAttribute.php
+++ app/Models/ItemAttribute.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItemAttribute extends Model
 {
+    protected $fillable = array(
+    'name',
+    'value'
+  );
     public $timestamps  = false;
+    
+/**
+ * getting attridutes and it's name from Attribute class
+ * @method attributes_and_names
+ * @return Array         return array with values and names of attributes
+ */
+    public function attributes_and_names()
+    {
+        return $this->hasOne(Attribute::class,'id','attr_id');
+    }
+
+/**
+ * Just taking attributes and own values without name
+ * @method attributes
+ * @return Array
+ */
+    public function attributes()
+    {
+        return $this->belongsTo(Item::class,'id');
+    }
 }

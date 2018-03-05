f5c3dcae1ff00298be97419b6770eadec0798989
Vadim <vadim@hyperweb.com.ua>
2018-03-05 20:42:41 +0200

    added script 4 rename (update) of attributes in 2 languages

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -1,6 +1,6 @@
 $(function(){
   // Brands. Change name
-  $(".change_name").on("click", function(event){
+  $(".change-brand-name").on("click", function(event){
     event.preventDefault();
     var old_name = $(this).attr('data-name');
     var name = prompt('Введіть нову назву', old_name);
@@ -15,6 +15,7 @@ $(function(){
     else return false;
   });
   // var old_id=null;
+    /* Cstegory rename. Old method
   $(".change-category").on("click", function(event){
         // event.preventDefault();
         var old_name = $(this).attr('data-name');
@@ -33,7 +34,18 @@ $(function(){
         // }
         // old_id=id;
         $("#parent-select").val(sub_id).change();
-      })
+      })*/
+    // update attributes
+    $(".change-attr-name").on('click',function () {
+        // event.preventDefault();
+        $('.edit-attrs').removeClass('d-none');
+        var old_ru = $(this).attr("data-lang-ru");
+        var old_uk = $(this).attr("data-lang-uk");
+        var id = $(this).attr("id");
+        $("#uk-attr-name-ed").val(old_uk);
+        $("#ru-attr-name-ed").val(old_ru);
+        $("#id-edited").val(id);
+    });
   // add category checkbox
   $('.add_cat').on('click',function(event){
     event.preventDefault();

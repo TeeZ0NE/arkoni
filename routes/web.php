ff5c2432a5472d98abe0b5b15dd1181101e23a75
Vadim <vadim@hyperweb.com.ua>
2018-03-10 15:50:30 +0200

    Added routes 4 Category, SubCategory, search

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Http\Request;
 
 /*
@@ -18,30 +19,30 @@ Route::get('/', function () {
 });
 //TODO: Remove this route
 Route::get('some{id?}{ram?}', function (Request $request) {
-    echo 'request '.$request->input('id');
+    echo 'request ' . $request->input('id');
     // echo '<br>'.$request->input('ram','default');
     // print_r($request->all());
     // echo "/n/r\n\r\t".$request->query('ram','defA').PHP_EOL.'<br>';
     if ($request->exists('ram')) {
-        echo "<br>_ram=".$request->ram.'<br>';
+        echo "<br>_ram=" . $request->ram . '<br>';
         if (isset(json_decode($request->ram)->k2)) {
-            echo "<p>".json_decode($request->ram)->k2."</p>";
+            echo "<p>" . json_decode($request->ram)->k2 . "</p>";
         } else {
             echo '<p>not set k2</p>';
         };
     }
     foreach ($request->all() as $key => $value) {
-        echo $key.'=='.$value;
+        echo $key . '==' . $value;
     }
     Cookie::queue('nameKey', 'Myvalue2', 2);
-    echo ($request->session()->exists('mykey'))?'key is '.session('mykey'):'hasnt key';
-    echo '<br>name='.$request->cookie('name');
+    echo ($request->session()->exists('mykey')) ? 'key is ' . session('mykey') : 'hasnt key';
+    echo '<br>name=' . $request->cookie('name');
     $request->flash();
-    echo '<br> old name is='.$request->old('name').'<br>';
+    echo '<br> old name is=' . $request->old('name') . '<br>';
     //return response('Hello World', 200)->header('Content-Type', 'text/plain');
-    session(['mykey'=>'som V2']);
+    session(['mykey' => 'som V2']);
     // print_r($request->session()->all());
-    echo ($request->session()->has('mykey'))?'key is '.session('mykey'):'hasnt key';
+    echo ($request->session()->has('mykey')) ? 'key is ' . session('mykey') : 'hasnt key';
     // Log::info('some info appear here',["messag"=>"ohoho"]);
 })->middleware('auth');
 
@@ -49,8 +50,6 @@ Route::get('some{id?}{ram?}', function (Request $request) {
 Auth::routes();
 // Logining Admin(s)
 Route::get('/home', 'HomeController@index')->name('home');
-
-
 
 
 Route::prefix('admin')->group(function () {
@@ -65,49 +64,39 @@ Route::prefix('admin')->group(function () {
         Route::get('/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.resset');
     });
     // Categories
-    Route::prefix('/categories')->group(function(){
-        Route::get('/','CategoryController@index')->name('cats.index');
-        Route::get('/delete/{cat}','CategoryController@destroy')->name('cat.destroy');
-        Route::get('/{id}/edit','CategoryController@edit')->name('cat.edit');
-        Route::group(['middleware'=>['purify']], function () {
-            Route::get('/create','CategoryController@create')->name('cats.create');
+    Route::prefix('/categories')->group(function () {
+        Route::get('/', 'CategoryController@index')->name('cats.index');
+        Route::get('/delete/{cat}', 'CategoryController@destroy')->name('cat.destroy');
+        Route::get('/{id}/edit', 'CategoryController@edit')->name('cat.edit');
+        Route::group(['middleware' => ['purify']], function () {
+            Route::get('/create', 'CategoryController@create')->name('cats.create');
             Route::get('/query', 'CategoryController@search')->name('cats.search');
-            Route::post('/update','CategoryController@update')->name('cat.update');
+            Route::post('/update', 'CategoryController@update')->name('cat.update');
+            Route::post('/store', 'CategoryController@store')->name('cat.store');
         });
     });
-    /*Route::prefix('/categories')->group(
-        function () {
-        Route::get('/', 'CategoriesController@index')->name('cats');
 
-        Route::group(['middleware'=>['purify']], function () {
-            Route::get('/query', 'CategoriesController@search')->name('cats.search');
-            Route::post('/', 'CategoriesController@store')->name('cat.store');
-            Route::post('/renname/query', 'CategoriesController@update')->name('cat.update');
-        });
-
-        Route::get('/delete/{id}', 'CategoriesController@delete')->name('cat.delete');
-    });*/
-
-   // SubCategory
-    Route::resource('subcategory','SubCategoryController');
+    // SubCategory
+    Route::group(['middleware' => ['purify']], function () {
+        Route::get('subcategory/query','SubCategoryController@search')->name('subcategory.search');
+        Route::resource('subcategory', 'SubCategoryController');
+    });
     // Brands
     Route::prefix('/brands')->group(function () {
         Route::get('/', 'BrandController@index')->name('brands');
 
-        Route::group(['middleware'=>['purify']], function () {
+        Route::group(['middleware' => ['purify']], function () {
             Route::get('/query', 'BrandController@search')->name('brands.search');
             Route::post('/', 'BrandController@store')->name('brand.store');
             Route::get('/update/query', 'BrandController@update')->name('brand.update');
         });
-
         Route::get('/delete/{id}', 'BrandController@delete')->name('brand.delete');
     });
 
     // Attributes
     Route::prefix('/attributes')->group(function () {
         Route::get('/', 'AttributesController@index')->name('attrs');
-
-        Route::group(['middleware'=>['purify']], function () {
+        Route::group(['middleware' => ['purify']], function () {
             Route::get('/query', 'AttributesController@search')->name('attrs.search');
             Route::post('/', 'AttributesController@store')->name('attr.store');
             Route::post('/update', 'AttributesController@update')->name('attr.update');

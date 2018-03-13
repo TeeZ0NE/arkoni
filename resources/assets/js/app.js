3bc9a618888ecb89e215b293e4f4b330f5dc6b8d
Vadim <vadim@hyperweb.com.ua>
2018-03-13 14:31:04 +0200

    commited library markItUp because it doesn't working now

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,10 @@
 var $ = require("jquery");
 require('popper.js');
 require('bootstrap');
+// import MarkItUp from 'markitup';
+// require('markitup');
+
+
 // require('tablesorter');
 // TODO: delete me
 console.log('all loaded');

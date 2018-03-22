4999a481dca0e33d125936c2f07aa9995bc48dfa
Vadim <vadim@hyperweb.com.ua>
2018-03-22 19:43:06 +0200

    Created migrations 4 tags.

---
+++ database/migrations/2018_03_22_170152_cr_uk_tags.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrUkTags extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uk_tags', function (Blueprint $table) {
+            $table->unsignedInteger('tag_id');
+            $table->string('uk_name')->charset('utf8')->comment('tag\'s name');
+            $table->string('title', 70)->charset('utf8')->comment('page title');
+            $table->string('description')->charset('utf8')->nullable()->comment('page description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uk_tags');
+    }
+}

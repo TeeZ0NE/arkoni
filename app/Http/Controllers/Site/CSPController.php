2f3db4a2695ca8b5d7dc7e86e5076520e6e6b1b8
Vadim <vadim@hyperweb.com.ua>
2018-04-12 14:08:59 +0300

    Deleted todo from tag view and SCPcontroller

--- app/Http/Controllers/Site/CSPController.php
+++ app/Http/Controllers/Site/CSPController.php
@@ -171,9 +171,6 @@ class CSPController extends BaseController
             'title' => $tag[$tag_method]->title,
             'description' => $tag[$tag_method]->description,
             'tags'=>$this->tagCombine($data['items']),
-            //TODO:: do are need?
-            'tag_method' => $tag_method,
-            'tag' => $tag,
         ]);
     }
 
